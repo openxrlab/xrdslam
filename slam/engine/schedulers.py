d270817890d47816774d36b5b292942598884943
wangxiaomeng <wangxiaomeng@sensetime.com>
2024-03-21 15:24:16 +0800

    copyright

--- slam/engine/schedulers.py
+++ slam/engine/schedulers.py
@@ -1,4 +1,3 @@
-# Copyright (c) OpenMMLab. All rights reserved.
 """Scheduler Classes."""
 
 from abc import abstractmethod
--- slam/engine/xrdslamer.py
+++ slam/engine/xrdslamer.py
@@ -1,4 +1,3 @@
-# Copyright (c) OpenMMLab. All rights reserved.
 from __future__ import annotations
 
 from dataclasses import dataclass, field
